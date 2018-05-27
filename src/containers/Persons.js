c1b2a3626f4b62e384f0be87e9dc33ee26863255
jiraphon <jiraphon@sirimedia.com>
2018-05-27 17:58:08 +0700

    Dispatching onAddPerson

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -3,22 +3,13 @@ import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionType from '../store/actions';
 
 class Persons extends Component {
     state = {
         persons: []
     }
 
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
 
     personDeletedHandler = (personId) => {
         this.setState( ( prevState ) => {
@@ -29,8 +20,8 @@ class Persons extends Component {
     render () {
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
+                <AddPerson personAdded={this.props.onAddPerson} />
+                {this.props.prs.map(person => (
                     <Person 
                         key={person.id}
                         name={person.name} 
@@ -43,9 +34,16 @@ class Persons extends Component {
 }
 
 const mapStateToProps = state => {
+    console.log('state: ', state);
     return {
         prs: state.persons
     }
 }
 
-export default connect(mapStateToProps)(Persons);
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPerson: () => dispatch({type: actionType.ADD_PERSON})
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,24 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  console.log('action.type: ', action.type);
+  switch (action.type) {
+    case actionType.ADD_PERSON:
+      const newPerson = {
+        id: Math.random(), // not really unique but good enough here!
+        name: 'Max',
+        age: Math.floor( Math.random() * 40 )
+      };
+      return { 
+        ...state,
+        persons: state.persons.concat( newPerson )
+      };
+      
+    default:
+      break;
+  
+  }
+
   return  state;
 };
 
